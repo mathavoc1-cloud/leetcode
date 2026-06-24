741e33b5ab9ba4f3eea07cfd6e5d5417db19ae60
mathavoc1-cloud <mathavoc1@gmail.com>
2026-06-24 18:20:46 +1000

    Update ex001.py

--- ex001/ex001.py
+++ ex001/ex001.py
@@ -1,36 +1,23 @@
 #declaracao da classe
-class Gafanhoto: 
-    def __init__(self): #-> metodo construtor:
+class Perfil:
+    def __init__(self): #metodo construtor
         
     #atributos
         self.nome = ""
         self.idade = 0
 
-
     #metodos de instancia
     def aniversario(self):
         self.idade += 1
+    
     def mensagem(self):
-        return f'{self.nome} eh um(a) Gafanhoto(a) e tem {self.idade} anos de idade'
-
-
-
+        return f'Ola {self.nome}, voce tem {self.idade} anos de idade!'
+   
 
 #declaracao dos objetos
-obj = Gafanhoto() #<- chamada de instanciacao
-obj.nome = 'Maria'
-obj.idade = 27 #-> n tem parenteses, eh um atributo
-obj.aniversario() #-> tem parenteses, logo eh um metodo
-print(obj.mensagem())
+p = Perfil()#<- chamada de instanciacao
+p.nome = 'Matheus'
+p.idade = 27 #-> n tem parenteses, eh um atributo
+p.aniversario()#-> tem parenteses, logo eh um metodo
 
-
-g2 = Gafanhoto ()
-g2.nome = "Mauro"
-g2.idade = 33
-g2.aniversario()
-print(g2.mensagem())
-
-g3 = Gafanhoto()
-g3.nome = "Matheus"
-g3.idade = 27
-print(g3.mensagem())
+print(p.mensagem())
