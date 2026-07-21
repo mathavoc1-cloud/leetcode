2794da4e6694b8ff674cd264edf1942c12173375
mathavoc1-cloud <mathavoc1@gmail.com>
2026-07-21 21:51:50 +1000

    Update desafio006.py

--- desafios/desafio006.py
+++ desafios/desafio006.py
@@ -5,7 +5,12 @@ from rich import print
 
 class Caneta: 
 
-    def __init__(self, caneta_cor):
-        self.caneta_cor = []
+    def __init__(self, cor):
+        self.cor = cor
+
+    def escrever(self, frase):
+        print(f'[{self.cor}]{frase}[/{self.cor}]')
 
         
+f1 = Caneta(cor = 'yellow')
+f1.escrever('Ola, como vai voce? ')
