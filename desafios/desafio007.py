071abc5295c8db663730f97ef6233d60b468f02e
mathavoc1-cloud <mathavoc1@gmail.com>
2026-07-25 15:52:41 +1000

    Update desafio007.py

--- desafios/desafio007.py
+++ desafios/desafio007.py
@@ -6,18 +6,90 @@ from rich.text import Text
 
 
 class ControleRemoto:
+    canal_min: int = 1
+    canal_max: int = 6
+    volume_min: int = 1
+    volume_max: int= 6
 
-    def __init__(self, canal=0, volume = 0, liga = False, desliga = True):
-        self.canal = canal
-        self.volume = volume
-        self.liga = liga
-        self.desliga = desliga
+    def __init__(self, canal= 1, volume = 0):
+        self.canal_atual: int = canal
+        self.volume_atual: int = volume
+        self.ligado: bool = False
 
-    def etiqueta(self):
-        conteudo = Text(justify = "left")
-        if self.liga == True:
-            conteudo.append("A TV esta desligada")
+    def liga_desliga(self):
+        self.ligado = not self.ligado   
+
+    def canal_mais(self):
+        if self.ligado:
+            if self.canal_atual == ControleRemoto.canal_max:
+                self.canal_atual = ControleRemoto.canal_min
+            else:
+                self.canal_atual += 1
+
+    def canal_menos(self):
+        if self.ligado:
+            if self.canal_atual == ControleRemoto.canal_min:
+                self.canal_atual = ControleRemoto.canal_max
+            else:
+                self.canal_atual -= 1
+
+    def volume_mais(self):
+        if self.ligado:
+            if self.volume_atual == ControleRemoto.volume_max:
+                self.volume_atual = ControleRemoto.volume_min
+            else: 
+                self.volume_atual += 1
+
+    def volume_menos(self):
+        if self.ligado:
+            if self.volume_atual == ControleRemoto.volume_min:
+                self.volume_atual = ControleRemoto.volume_max
+            else:
+                self.volume_atual -= 1
+
+    def mostar_tv(self):
+
+        conteudo = ''
+        if not self.ligado:
+            conteudo = f':prohibited: [red] A TV esta desligada[/red]'
         else:
-            conteudo.append("A TV esta ligada")
+            conteudo = f'CANAL  = '
+            for canal in range (ControleRemoto.canal_min, ControleRemoto.canal_max +1):
+                if canal == self.canal_atual:
+                    conteudo += f'[black on yellow] {canal} [/]'
+                else:
+                    conteudo += f" {canal} "
 
+            conteudo += f"\nVOLUME = "
+
+            for volume in range (ControleRemoto.volume_min, ControleRemoto.volume_max +1):
+                if volume <= self.volume_atual:
+                    conteudo += f'[black on cyan] [/]'
+                else:
+                    conteudo += f"[black on white] [/] "
+
+        tv = Panel(conteudo, title="[ TV ]", width = 30)
+        print(tv)
+
+
+c = ControleRemoto(canal = 3, volume = 4)
+while True:
+    c.mostar_tv()
+    comando = str(input(f"\n < CH >  - VOL + "))
+    match comando:
+        case '0':
+            break
+        case '@':
+            c.liga_desliga()
+        case ">":
+            c.canal_mais()
+        case"<":
+            c.canal_menos()
+        case "-":
+            c.volume_menos()
+        case "+":
+            c.volume_mais()
+
+
+  
     
